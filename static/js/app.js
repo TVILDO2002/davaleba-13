0a8336a720e0ab018f98a8892a32f33c92e3596c
TVILDO2002 <72739978+TVILDO2002@users.noreply.github.com>
2023-03-23 20:25:19 +0400

    ..

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,6 @@
 const navLinks = document.getElementById("nav-links");
 const navBurger = document.getElementById("nav-burger");
+const modal = document.getElementById("modal-wrapper");
 
 function navBarActiveHandler() {
   document.documentElement.scrollTop = 0;
@@ -7,5 +8,11 @@ function navBarActiveHandler() {
   navLinks.classList.toggle("nav-links__active");
 }
 
-// events
+
 navBurger.addEventListener("click", navBarActiveHandler);
+
+function myFunction(){
+    modal.classList.toggle("active");
+}
+
+
